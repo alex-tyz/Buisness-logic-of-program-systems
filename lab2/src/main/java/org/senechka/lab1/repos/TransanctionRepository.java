6af8566fb7e0bc414a4689cd3a892525a0eeb666
alex-tyz <165581986+alex-tyz@users.noreply.github.com>
2025-03-13 15:08:19 +0300

    Добавлен метод findTransactionsByState для поиска транзакций по статусу в TransactionRepository

--- lab2/src/main/java/org/senechka/lab1/repos/TransanctionRepository.java
+++ lab2/src/main/java/org/senechka/lab1/repos/TransanctionRepository.java
@@ -1,11 +1,11 @@
 package org.senechka.lab1.repos;
 
 import org.senechka.lab1.models.Transaction;
-import org.senechka.lab1.models.UserTickets;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TransanctionRepository extends CassandraRepository<Transaction, String> {
@@ -19,5 +19,8 @@ public interface TransanctionRepository extends CassandraRepository<Transaction,
     void setTransactionStatusToFailed(@Param("id") UUID id);
 
     @Query("UPDATE transaction set link = :link WHERE id = :id")
-    void setTransactionLink(@Param("id") UUID id, @Param("link")String link);
+    void setTransactionLink(@Param("id") UUID id, @Param("link") String link);
+
+    @Query("SELECT * FROM transaction WHERE state = :state ALLOW FILTERING")
+    List<Transaction> findTransactionsByState(@Param("state") String state);
 }
