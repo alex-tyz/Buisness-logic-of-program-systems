54f7fb7afbdd53681d6d2b8c75f45af192564b6d
alex-tyz <165581986+alex-tyz@users.noreply.github.com>
2025-03-13 15:11:30 +0300

    Добавлено логирование старта приложения в Lab1Application

--- lab2/src/main/java/org/senechka/lab1/Lab1Application.java
+++ lab2/src/main/java/org/senechka/lab1/Lab1Application.java
@@ -2,12 +2,17 @@ package org.senechka.lab1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Lab1Application {
 
+	private static final Logger logger = LoggerFactory.getLogger(Lab1Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Lab1Application.class, args);
+		logger.info("Lab1 application started successfully.");
 	}
 
 }
